22903b3414e69bb7189a84b7a2fda22d602e04d9
le-github-de-yannick <jespiegl@uoguelph.ca>
2021-03-10 21:01:34 -0500

    Created POST/GET Profiles API

---
+++ stickit/routes/db/Profiles.js
@@ -0,0 +1,14 @@
+const profiles = [
+    {
+        user_id: 1,
+        first_name: "Yannick",
+        last_name: "Espiegle"
+    },
+    {
+        user_id: 2,
+        first_name: "John",
+        last_name: "Doe"
+    }
+];
+
+module.exports = profiles;

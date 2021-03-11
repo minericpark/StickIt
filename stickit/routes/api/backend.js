69c9a6368c91be0aa991a0b40018d42e611e43ab
detols <mpsericpark@gmail.com>
2021-03-10 21:51:36 -0500

    Added accounts API with Patch method and Accounts.js database

--- stickit/routes/api/backend.js
+++ stickit/routes/api/backend.js
@@ -1,10 +1,13 @@
 const express = require('express');
+const bodyParser = require('body-parser');
+
 const app = express();
 
-app.get("/", (req,res)=>{
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
 
-    res.send(); 
-});
+app.use('/profiles', require('./profiles_api'));
+app.use('/accounts', require('./accounts_api'));
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, ()=>console.log("Server is running on port "+PORT));
---
+++ stickit/routes/db/Accounts.js
@@ -0,0 +1,12 @@
+var accounts = [
+    {
+        user_id: 1,
+        password: "fds@34sa#@"
+    },
+    {
+        user_id: 2,
+        password: "johnDoePassword"
+    }
+];
+
+module.exports = accounts;

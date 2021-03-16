d9f4f101c8cb73e343be55a98857890e2cee5a82
detols <mpsericpark@gmail.com>
2021-03-15 22:06:50 -0400

    Added documentation for accounts_api patch

--- stickit/routes/api/accounts_api.js
+++ stickit/routes/api/accounts_api.js
@@ -5,6 +5,12 @@ const router = express.Router();
 
 const idFilter = req => account => account.user_id === req.params.id;
 
+/*
+method: PATCH
+required parameters: id -> string
+request body: new_password
+response: 200 OK; 400 Error
+*/
 router.patch('/:id', (req, res) => {
     const foundAccount = accounts.find(idFilter(req));
     const newPassword = req.body.new_password;

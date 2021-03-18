92f69370732769b765a5da7b2543131bde57d9af
Jonah Ruch <jonah.ruch@gmail.com>
2021-03-18 16:53:23 -0400

    routes /login and /createUser should be ready

--- stickit/routes/db/Accounts.js
+++ stickit/routes/db/Accounts.js
@@ -1,12 +1,19 @@
 const accounts = [
     {
         user_id: 1,
+        email : "test@example.ca",
         password: "fds@34sa#@"
     },
     {
         user_id: 2,
+        email : "test2@example.ca",
         password: "johnDoePassword"
-    }
+    },
+	{
+		user_id : "3",
+		email : "test3@example.ca",
+		password : "qwerty123"
+	}
 ];
 
 module.exports = accounts;

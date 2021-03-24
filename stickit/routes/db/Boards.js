96b09faebcc6d3fc19bbc89991b3480de3a8c216
Jonah Ruch <jonah.ruch@gmail.com>
2021-03-24 19:27:33 -0400

    template board backend

---
+++ stickit/routes/db/Boards.js
@@ -0,0 +1,35 @@
+const boards = [
+	{
+		user_id: "test@example.ca",
+		board_id : "board_1",
+		title: "my first board",
+		status : "Active"
+	},
+	{
+		user_id : "test2@example.ca",
+		board_id : "board_1",
+		title: "here is a title",
+		status : "Active"
+	},
+	{
+		user_id : "test3@example.ca",
+		board_id : "board_1",
+		title: "title of subject",
+		status : "Active"
+	},
+	{
+		user_id : "test3@example.ca",
+		board_id : "board_2",
+		title: "title of another subject",
+		status : "Active"
+	},
+	{
+		user_id : "test3@example.ca",
+		board_id : "board_3",
+		title: "title of a different subject",
+		status : "Active"
+	},
+
+ ];
+ 
+ module.exports = boards;

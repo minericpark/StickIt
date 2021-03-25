55b8396f027e5928a57eb2cb8f0525c1b4186d3a
Jonah Ruch <jonah.ruch@gmail.com>
2021-03-25 19:46:12 -0400

    changed to test for saved items in trash

--- stickit/routes/db/Boards.js
+++ stickit/routes/db/Boards.js
@@ -3,37 +3,37 @@ const boards = [
 		user_id: "test@example.ca",
 		board_id : "board_1",
 		title: "my first board",
-		status : "Active"
+		status : true
 	},
 	{
 		user_id : "test2@example.ca",
 		board_id : "board_1",
 		title: "here is a title",
-		status : "Active"
+		status : true
 	},
 	{
 		user_id : "test3@example.ca",
 		board_id : "board_1",
 		title: "title of subject",
-		status : "Active"
+		status : true
 	},
 	{
 		user_id : "test3@example.ca",
 		board_id : "board_2",
 		title: "title of another subject",
-		status : "Active"
+		status : true
 	},
 	{
 		user_id : "test3@example.ca",
 		board_id : "board_3",
 		title: "title of a different subject",
-		status : "Active"
+		status : false
 	},
 	{
 		user_id : "johndoe@gmail.com",
 		board_id : "board_1",
 		title: "title of a different subject",
-		status : "Active"
+		status : true
 	},
 ];
  

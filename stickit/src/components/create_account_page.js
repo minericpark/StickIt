1192b3cdb85933abb568a7af5162e21b678e9699
Melissa Chinnick <bluerose.mcc@rogers.com>
2021-03-30 19:18:38 -0400

    Remove unused error states

--- stickit/src/components/create_account_page.js
+++ stickit/src/components/create_account_page.js
@@ -11,14 +11,6 @@ function CreateAccountPage() {
     const [password, setPassword] = useState('');
     const [fname, setFname] = useState('');
     const [lname, setLname] = useState('');
-    const [error, toggleError] = useState({
-        fname: false,
-        lname: false,
-        dob: false,
-        email: false,
-        password: false,
-    });
-    const [errorMsg, setError] = useState('');
 
     if (userID !== null) {
         return <Redirect to="/dashboard" />;
@@ -30,40 +22,17 @@ function CreateAccountPage() {
      */
     function handleCreate(event) {
         let success = true;
-        let errMsg = '';
 
         event.preventDefault();
 
         if (fname.length === 0) {
             success = false;
-            errMsg = 'Please enter your first name';
-            toggleError({ fname: true });
         } else if (lname.length === 0) {
             success = false;
-            errMsg = 'Please enter your last name';
-            toggleError({ lname: true });
         } else if (email.length === 0) {
             success = false;
-            errMsg = 'Please enter your email';
-            toggleError({ email: true });
         } else if (password.length === 0) {
             success = false;
-            errMsg = 'Please enter your password';
-            toggleError({ password: true });
-        } else {
-            toggleError({
-                email: false,
-                password: false,
-                fname: false,
-                lname: false,
-            });
-        }
-
-        // TODO: submit the login information for confirmation
-
-        if (!success) {
-            setError(errMsg);
-            return;
         }
 
         // First Send login request, if failed then create new profile
@@ -120,43 +89,24 @@ function CreateAccountPage() {
     function handleInput(event) {
         let name = event.target.name;
         let value = event.target.value;
-        let errMsg = '';
 
         if (name === 'fname') {
             if (value.trim() !== '') {
                 setFname(value);
-                console.log(value);
-            } else {
-                toggleError({ fname: true });
-                setError('Enter a first name');
             }
         } else if (name === 'lname') {
             if (value.trim() !== '') {
                 setLname(value);
-                console.log(value);
-            } else {
-                toggleError({ lname: true });
-                setError('Enter a last name');
             }
         } else if (name === 'email') {
             if (value.trim() !== '') {
                 setEmail(value);
-                console.log(value);
-            } else {
-                toggleError({ email: true });
-                setError('Enter an email');
             }
         } else if (name === 'password') {
             if (value.trim() !== '') {
                 setPassword(value);
-                console.log(value);
-            } else {
-                toggleError({ password: true });
-                setError('Enter a password');
             }
         }
-
-        setError(errMsg);
     }
     return (
         <div id="create_account_page" className="create-account-page">
@@ -189,8 +139,6 @@ function CreateAccountPage() {
                     size="small"
                     margin="dense"
                     variant="outlined"
-                    // error={error.email}
-                    // helperText={error.email ? errorMsg : null}
                     onChange={handleInput}
                     required={true}
                 />
@@ -201,8 +149,6 @@ function CreateAccountPage() {
                     size="small"
                     margin="dense"
                     variant="outlined"
-                    // error={error.password}
-                    // helperText={error.password ? errorMsg : null}
                     onChange={handleInput}
                     required={true}
                 />
